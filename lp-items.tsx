99f41c5d210e41ee66ac17e3126018dbcb091a1f
n232122321 <120481806+zskizz@users.noreply.github.com>
2024-11-25 23:19:22 +0100

    Update lp-items.tsx

--- lp-items.tsx
+++ lp-items.tsx
@@ -1,6 +1,6 @@
 export const LP_GRID_ITEMS = [
   {
-    title: "Inglés",
+    title: "Ingles",
     description: "Un sistema optimizado que utiliza las mejores prácticas para garantizar que todo funcione perfectamente, especialmente en inglés.",
     icon: (
       <svg
